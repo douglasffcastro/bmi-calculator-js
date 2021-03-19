945a06c2c152a1a1b25368637924aaaf493d1bb3
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-19 20:23:55 -0300

    :arrow-up: Update script

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#form')
 
 form.addEventListener('submit', function (e) {
   e.preventDefault()
+
   const inputHeight = e.target.querySelector('#height')
   const inputWeight = e.target.querySelector('#weight')
 
@@ -9,33 +10,60 @@ form.addEventListener('submit', function (e) {
   const height = Number(inputHeight.value)
 
   if (!height || !weight) {
-    setBMI('Please enter numbers only.')
+    setBmi('Please enter numbers only.')
+
     return
   }
 
-  const bmi = getBMI(weight, height)
-  const BMICategory = getBMICategory(bmi)
+  if (weight > 600 || weight < 0) {
+    setBmi('Please enter a valid weight.')
 
-  const msg = `BMI ${bmi}. Your result suggests you are ${BMICategory}`
+    return
+  }
 
-  setBMI(msg, true)
+  if (height > 3 || height < 0) {
+    setBmi('Please enter a valid height.')
+
+    return
+  }
+
+  const bmi = getBmi(weight, height)
+  const BmiCategory = getBmiCategory(bmi)
+  const msg = getMsg(bmi, BmiCategory)
+
+  setBmi(msg, true)
 })
 
-function getBMICategory (bmi) {
+function getBmiCategory (bmi) {
   const category = [
     'Underweight. Being underweight could be a sign you\'re not eating enough or you may be ill. If you\'re underweight, a GP can help.',
+
     'Healthy weight. Keep up the good work!',
+
     'Overweight. The best way to lose weight if you\'re overweight is through a combination of diet and exercise.',
-    'Obese. The best way to lose weight if you\'re obese is through a combination of diet and exercise, and, in some cases, medicines. See a GP for help and advice.'
+
+    'Moderately obese (Obese Class I). The best way to lose weight if you\'re obese is through a combination of diet and exercise, and, in some cases, medicines. See a GP for help and advice.',
+
+    'Severely obese (Obese Class II). The best way to lose weight if you\'re obese is through a combination of diet and exercise, and, in some cases, medicines. See a GP for help and advice.',
+
+    'Very severely obese (Obese Class III). The best way to lose weight if you\'re obese is through a combination of diet and exercise, and, in some cases, medicines. See a GP for help and advice.'
   ]
 
+  if (bmi >= 40) return category[5]
+  if (bmi >= 35) return category[4]
   if (bmi >= 30) return category[3]
   if (bmi >= 25) return category[2]
   if (bmi >= 18.5) return category[1]
   if (bmi < 18.5) return category[0]
 }
 
-function getBMI (weight, height) {
+function getMsg (bmi, category) {
+  const msg = `BMI ${bmi}. Your result suggests you are ${category}`
+
+  return msg
+}
+
+function getBmi (weight, height) {
   const bmi = weight / height ** 2
 
   return bmi.toFixed(2)
@@ -43,15 +71,17 @@ function getBMI (weight, height) {
 
 function createP () {
   const p = document.createElement('p')
+
   return p
 }
 
 function createH4 () {
   const h4 = document.createElement('h4')
+
   return h4
 }
 
-function setBMI (msg, isValid) {
+function setBmi (msg, isValid) {
   const titleBox = document.querySelector('.title-box')
   const result = document.querySelector('.result')
 
@@ -63,7 +93,9 @@ function setBMI (msg, isValid) {
 
   if (isValid) {
     h4.innerHTML = 'Your BMI'
+
     titleBox.appendChild(h4)
+
     p.classList.add('result-paragraph')
   } else {
     p.classList.add('bad')
